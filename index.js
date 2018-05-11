4c34ea5cf5ce79054dd529fb16a8ebe9e30eba36
efer-git <36735912+efer-git@users.noreply.github.com>
2018-05-11 14:34:15 +0900

    update index.
    check whether event is valid

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
+//const request = require('request');
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
+
+exports.handler = function(event, context, callback) {
+  //let contents = fs.readFileSync(`public${path.sep}index.html`);
+  console.log(checkQuery(event.query))
+  let result = {
     statusCode: 200,
-    body: contents.toString(),
+    body: checkQuery(event.query),
     headers: {'content-type': 'text/html'}
   };
 
   callback(null, result);
 };
+function checkQuery(query){ //check is query project_type : PRJNA012312 ERP12312 mgp100231
+	let reProject = new RegExp("(^PRJ(NA|EB|DA|DB)[0-9]+|^(E|S|D)RP[0-9]+|^mgp[0-9]+)","g");
+	return reProject.test(query)
+}
