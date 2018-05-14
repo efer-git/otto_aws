3196753c7e89f2005a7b3e4bdde0e45b1cbcda78
efer-git <36735912+efer-git@users.noreply.github.com>
2018-05-14 15:56:45 +0900

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -5,7 +5,7 @@ var index = require('../index.js');
 
 describe('Tests index', function() {
   it('verifies successful response', function(done) {
-    index.get({"query":"PRJ33" }, { /* context */ }, (err, result) => {
+    index.get({"query":"PRJEB20022" }, { /* context */ }, (err, result) => {
       try {
         test.number(result.statusCode).is(200);
         test.string(result.body).contains('Congratulations');
